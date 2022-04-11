23ed742898f2e27dc322d1d4024f2bd24f7960c7
hemms87 <hemanth.infosys@gmail.com>
2022-04-11 12:42:52 +0100

    Create User save changes

--- src/pages/CreateJob.js
+++ src/pages/CreateJob.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import {
   Form,
-  FormControl,
   Container,
   Button,
   Col,
--- src/pages/CreateUser.js
+++ src/pages/CreateUser.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Form,
-  FormControl,
   Container,
   Button,
   Col,
@@ -9,11 +8,69 @@ import {
 } from "react-bootstrap";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { saveUser } from "../controllers/UserActions";
+import { ToastError } from "../service/toast/Toast";
+import { ToastSuccess } from "../service/toast/Toast";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 export default function CreateUser() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [contact, setContact] = useState("");
+  const [line1, setLine1] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [zip, setZip] = useState("");
+  const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const numberPattern = new RegExp(/^[0-9\b]+$/);
+
+  const validateAndSaveUser = () => {
+    if (!userName.trim() || !line1.trim()
+      || !country.trim() || !state.trim() || !city.trim()
+      || !zip.trim() || !email.trim() || !password.trim() || !contact.trim()) {
+      ToastError("Please fill all required fields");
+    } else {
+      if ((email && !emailPattern.test(email))) {
+        ToastError("Please enter a valid email address");
+        return;
+      }
+      if ((contact && (!numberPattern.test(contact) || contact.length != 10))) {
+        ToastError("Please enter a valid contact number");
+        return;
+      }
+      let address = {
+        "line1": line1,
+        "city": city,
+        "state": state,
+        "country": country,
+        "zip": zip
+      }
+      let payload = {
+        "name": userName,
+        "email": email,
+        "password": password,
+        "contact": contact,
+        "address": address,
+      }
+      saveUser(payload, (data) => {
+        if (data.status === 'success') {
+          ToastSuccess("User Profile saved successfully");
+        } else {
+          ToastError(data.message);
+        }
+      }, (err) => {
+        ToastError("Unexpected error during User Profile Save");
+      });
+    }
+  }
+
   return (
     <>
       <Header admin />
+      <ToastContainer />
       <Container>
         <h1
           style={{ color: "#6f42c1" }}
@@ -26,41 +83,57 @@ export default function CreateUser() {
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Control
-                  className="shadow"
+                  className="shadow" value={userName}
+                  onChange={e => setUserName(e.target.value)}
+                  type="email"
+                  placeholder="Name"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control
+                  className="shadow" value={email}
+                  onChange={e => setEmail(e.target.value)}
                   type="email"
                   placeholder="Email Address"
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Control
-                  className="shadow"
+                  className="shadow" value={password}
+                  onChange={e => setPassword(e.target.value)}
                   type="password"
                   placeholder="Password"
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasiccontact">
                 <Form.Control
-                  className="shadow"
+                  className="shadow" value={contact}
+                  onChange={e => setContact(e.target.value)}
                   type="number"
                   placeholder="Contact number"
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicline1">
-                <Form.Control className="shadow" placeholder="Address Line" />
+                <Form.Control className="shadow" value={line1}
+                  onChange={e => setLine1(e.target.value)} placeholder="Address Line" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicState">
-                <Form.Control className="shadow" placeholder="State" />
+                <Form.Control className="shadow" value={state}
+                  onChange={e => setState(e.target.value)} placeholder="State" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicCity">
-                <Form.Control className="shadow" placeholder="City" />
+                <Form.Control className="shadow" value={city}
+                  onChange={e => setCity(e.target.value)} placeholder="City" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicCountry">
-                <Form.Control className="shadow" placeholder="Country" />
+                <Form.Control className="shadow" value={country}
+                  onChange={e => setCountry(e.target.value)} placeholder="Country" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicZip">
-                <Form.Control className="shadow" placeholder="Zip" />
+                <Form.Control className="shadow" value={zip}
+                  onChange={e => setZip(e.target.value)} placeholder="Zip" />
               </Form.Group>
-              <Button type="submit">Submit</Button>
+              <Button onClick={validateAndSaveUser}>Save</Button>
             </Form>
           </Col>
         </Row>
