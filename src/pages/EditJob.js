02091134016a0d8623deacc2c1a1175a225f5289
mariaayoub58 <mariaayoub.se@gmail.com>
2022-04-12 06:21:18 +0100

    Added Searchbar

--- src/pages/EditJob.js
+++ src/pages/EditJob.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import {
+  Card,
+  Button,
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
 import "../Style.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -10,12 +18,26 @@ export default function () {
     <>
       <Header admin />
       <Container>
+        <Col lg={10} md={6} sm={12} className="p-5 m-auto rounded-lg">
+          <InputGroup className="col-6 mt-5">
+            <FormControl
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-secondary" id="button-addon2">
+              Search
+            </Button>
+          </InputGroup>
+        </Col>
+        {/**
         <h1
           style={{ color: "#6f42c1", textShadow: "5px 5px 25px grey" }}
           className="mt-5 p-3  text-center rounded"
         >
           Job Listing
         </h1>
+         */}
         <Row>
           <Col lg={10} md={6} sm={12} className="p-5 m-auto rounded-lg">
             <CardListing
